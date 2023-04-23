bdffab84cc920f5fdf6f30f1b3f832125cec05b4
Wycliffe <wycliffekiprono4@gmail.com>
2023-04-23 13:03:59 +0300

    create Transaction component, use useEffect, useState

---
+++ src/components/Transaction.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+function Transaction() {
+    const [transactions, setTransactions] = useState([])
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/transactions")
+        .then((res) => res.json())
+        .then((data) => setTransactions(data))
+        .catch((error) => console.log(error))
+    }, [])
+
+    return (
+        <div>
+            <h2>Transactions</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.date}</td>
+                            <td>{transaction.description}</td>
+                            <td>{transaction.amount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default Transaction
